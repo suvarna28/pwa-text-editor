d04c3d2887a5e997898b9324a8f9dc8669f1e17c
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2023-04-03 22:04:00 -0400

    Pushing to Heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
 module.exports = () => {
   return {
     mode: 'development',
